1e1d03831cd4f79000e25c4b02a4380f91874ea7
andre <andre.sousabaptista@gmail.com>
2025-01-14 23:30:17 +0000

    [Equadis-1] Account springApplication

--- account-service/src/main/java/javachallengeequadiscustomer/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/javachallengeequadiscustomer/accountservice/AccountServiceApplication.java
@@ -2,10 +2,20 @@ package javachallengeequadiscustomer.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * The type Account service application.
+ */
 @SpringBootApplication
+@EnableFeignClients
 public class AccountServiceApplication {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
     }
