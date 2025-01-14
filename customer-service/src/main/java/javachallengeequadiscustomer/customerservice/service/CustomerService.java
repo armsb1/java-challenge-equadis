7bc88875bf8431432c45fa8848fc9591f4fbc717
andre <andre.sousabaptista@gmail.com>
2025-01-14 03:36:07 +0000

    [Equadis-1] Customer Service

---
+++ customer-service/src/main/java/javachallengeequadiscustomer/customerservice/service/CustomerService.java
@@ -0,0 +1,82 @@
+package javachallengeequadiscustomer.customerservice.service;
+
+import javachallengeequadiscustomer.customerservice.entity.Customer;
+import javachallengeequadiscustomer.customerservice.mapper.CustomerMapper;
+import javachallengeequadiscustomer.customerservice.model.CustomerDto;
+import javachallengeequadiscustomer.customerservice.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * The type Customer service.
+ */
+@Service
+@AllArgsConstructor
+public class CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    /**
+     * Find all customers list.
+     *
+     * @return the list
+     */
+    public List<CustomerDto> findAllCustomers() {
+        return customerRepository.findAll()
+                .stream()
+                .map(CustomerMapper::toCustomerDTO)
+                .toList();
+    }
+
+    /**
+     * Find customer by id customer dto.
+     *
+     * @param id the id
+     * @return the customer dto
+     */
+    public CustomerDto findCustomerById(Long id) {
+        return CustomerMapper.toCustomerDTO(customerRepository.findById(id).orElseThrow());
+    }
+
+    /**
+     * Find customer by user profile id customer dto.
+     *
+     * @param userProfileId the user profile id
+     * @return the customer dto
+     */
+    public CustomerDto findCustomerByUserProfileId(Long userProfileId) {
+        return CustomerMapper.toCustomerDTO(customerRepository.findByUserProfileId(userProfileId).orElseThrow());
+    }
+
+    /**
+     * Create customer customer.
+     *
+     * @param customerDto the customer dto
+     * @return the customer
+     */
+    public Customer createCustomer(CustomerDto customerDto) {
+        if (customerRepository.existsByEmail(customerDto.email())) {
+            throw new IllegalArgumentException("email already exists.");
+        }
+
+        Long userProfileId = customerDto.userProfileId() != null
+                ? customerDto.userProfileId()
+                : generateUserProfileId();
+
+        if (customerRepository.existsByUserProfileId(userProfileId)) {
+            throw new IllegalArgumentException("UserProfileId already exists.");
+        }
+
+        Customer customer = CustomerMapper.toCustomer(customerDto);
+        customer.setUserProfileId(userProfileId);
+
+        return customerRepository.save(customer);
+    }
+
+    private Long generateUserProfileId() {
+        return UUID.randomUUID().getLeastSignificantBits() & Long.MAX_VALUE;
+    }
+}
