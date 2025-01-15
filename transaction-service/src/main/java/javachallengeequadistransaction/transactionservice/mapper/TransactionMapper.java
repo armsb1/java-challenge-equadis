a0ae93ca4db123d8d1fc087ebe938d68495b6d84
andre <andre.sousabaptista@gmail.com>
2025-01-15 03:48:40 +0000

    [Equadis-1] Transaction mapper

---
+++ transaction-service/src/main/java/javachallengeequadistransaction/transactionservice/mapper/TransactionMapper.java
@@ -0,0 +1,42 @@
+package javachallengeequadistransaction.transactionservice.mapper;
+
+import javachallengeequadistransaction.transactionservice.entity.Transaction;
+import javachallengeequadistransaction.transactionservice.model.TransactionDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * The type Transaction mapper.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class TransactionMapper {
+
+    /**
+     * To transaction transaction.
+     *
+     * @param accountDto the account dto
+     * @return the transaction
+     */
+    public static Transaction toTransaction(TransactionDto accountDto) {
+        return Transaction.builder()
+                .accountNumber(accountDto.accountNumber())
+                .amount(accountDto.amount())
+                .comments(accountDto.comments())
+                .build();
+    }
+
+    /**
+     * To transaction dto transaction dto.
+     *
+     * @param account the account
+     * @return the transaction dto
+     */
+    public static TransactionDto toTransactionDTO(Transaction account) {
+        return TransactionDto.builder()
+                .accountNumber(account.getAccountNumber())
+                .amount(account.getAmount())
+                .comments(account.getComments())
+                .build();
+    }
+
+}
